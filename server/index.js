e054d10b3d27ec25b0ac727dc682da630f7a7859
owenunda <oweenunda.dev@gmail.com>
2025-09-07 01:00:32 -0500

    add: se agrego web sockets y envio de informacion del cliente

--- server/index.js
+++ server/index.js
@@ -1,14 +1,29 @@
 import express from 'express'
 import logger from 'morgan'
+
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
+
 const port = process.env.PORT ?? 3000
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server)
+
+io.on('connection', (socket)=>{
+  console.log('a user has connected - index.js:14')
+  
+  socket.on('disconnect', ()=>{
+    console.log('an user has disconnected - index.js:17');
+  })
+})
+
 app.use(logger('dev'))
 
 app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/cliente/index.html')
 })
 
-app.listen(port, ()=>{
-  console.log(`Server running on port ${port} - index.js:13`);
+server.listen(port, ()=>{
+  console.log(`Server running on port ${port} - index.js:28`);
 })
